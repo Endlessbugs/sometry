e5c9db40efcc530b4e348f2b4d64d7fa44db2eda
刘刘刘 <@DESKTOP-MPEJ5RM>
2021-07-08 16:31:22 +0800

    添加项目文件。

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+/*Don't mind
+I say
+    lol*/ 
+int x;
+int y;
+
+namespace minefunc {
+    void func()
+    {
+        cout <<"Fuck ";
+    }
+}
+namespace tf {
+    void em()
+    {
+        cout << "you!" << endl;
+    }
+}
+int main()
+{
+    cout << "x=";
+    cin >> x;
+    cout << "y=";
+    cin >> y;
+    auto sum = x + y;
+    sum += 7;
+    cout <<x + y<< endl;
+    cout <<sum<<endl;
+    system("pause");
+    return 0;
+}
