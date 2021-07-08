a05089da5f39a60c96dd6cc4605e0bcf49cc8c06
思瀚 刘 <L2412246965@hotmail.com>
2021-07-08 16:41:00 +0800

    添加项目文件。

---
+++ test.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    //your code goes here
+    int x;
+    int y = 50;
+    cout << "the number of passengers waiting in the bus station=";
+    cin >> x;
+    int n = x % y;
+    if (n==0)
+    {
+        cout << "the empty seat of the last bus is 0 ." << endl;
+
+    }
+    else
+    {
+        n = y - n;
+        cout << "the empty seat of the last bus is " << n << " ." << endl;
+        
+    }
+    
+}
